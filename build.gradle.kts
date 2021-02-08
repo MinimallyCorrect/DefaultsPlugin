5451a5e21417b2f9d3b46cb6b9acd8db7576c607
Luna <git@nyx.nova.fail>
2021-02-08 19:10:21 +0000

    Use gradle kotlin script

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+buildscript {
+	repositories { jcenter() }
+	dependencies { classpath("org.minimallycorrect.gradle:DefaultsPlugin:0.1.3") }
+}
+plugins {
+	id("java-gradle-plugin")
+	id("maven-publish")
+	id("com.gradle.plugin-publish") version "0.12.0"
+	id("com.jfrog.bintray") version "1.8.5"
+	id("org.shipkit.shipkit-auto-version") version "1.1.0"
+	id("org.shipkit.shipkit-changelog") version "0.2.21"
+	id("org.shipkit.shipkit-github-release") version "1.1.1"
+}
+
+group = "org.minimallycorrect.gradle"
+
+apply(from = "$rootDir/gradle/shipkit.gradle")
+
+repositories {
+	mavenCentral()
+	maven(url = "https://plugins.gradle.org/m2/")
+}
+
+configurations.all { resolutionStrategy.cacheChangingModulesFor(30, "seconds") }
+
+dependencies {
+	api(gradleApi())
+	compileOnly("org.jetbrains:annotations:20.1.0")
+	implementation("com.diffplug.spotless:spotless-plugin-gradle:5.9.0")
+	implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
+	annotationProcessor("com.github.bsideup.jabel:jabel-javac-plugin:0.3.0")
+}
+
+tasks.withType<Javadoc> {
+	(options as? CoreJavadocOptions)?.addStringOption("Xdoclint:none", "-quiet")
+}
+
+// per jabel docs
+tasks.withType<JavaCompile> {
+	options.compilerArgs.addAll(listOf(
+		"--release", "11", // Avoid using Java 9+ APIs
+		"--enable-preview"
+	))
+	// The following line can be omitted on Java 14 and higher
+	options.compilerArgs.add("-Xplugin:jabel")
+
+	doFirst {
+		options.compilerArgs.remove("--enable-preview")
+	}
+}
+
+gradlePlugin {
+	plugins {
+		create("DefaultsPlugin") {
+			id = "org.minimallycorrect.gradle.DefaultsPlugin"
+			implementationClass = "org.minimallycorrect.gradle.DefaultsPlugin"
+			description = "Sets up sensible defaults for our gradle projects to avoid boilerplate "
+		}
+	}
+}
+
+pluginBundle {
+	website = "http://www.minimallycorrect.org/"
+	vcsUrl = "https://github.com/MinimallyCorrect/DefaultsPlugin"
+	tags = listOf("gradle", "gradle-plugin")
+}
