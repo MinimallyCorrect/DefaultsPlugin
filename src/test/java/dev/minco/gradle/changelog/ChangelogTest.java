bb1aff89c8221d68bb18a70fee0a4d85d2cf61fb
Luna <git@nyx.nova.fail>
2021-02-18 00:01:27 +0000

    Add assumption

--- src/test/java/dev/minco/gradle/changelog/ChangelogTest.java
+++ src/test/java/dev/minco/gradle/changelog/ChangelogTest.java
@@ -1,6 +1,7 @@
 package dev.minco.gradle.changelog;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -8,6 +9,8 @@ import java.io.IOException;
 public class ChangelogTest {
 	@Test
 	public void testCommitExtraction() throws IOException {
+		Assumptions.assumeTrue(System.getenv("CI") == null, "Running on CI, full history isn't available");
+
 		var result = ChangelogBuilder.extractCommits("5402843f1e78c477f10c6", "32fa725a96484402d0");
 		Assertions.assertEquals("- Remove bintray from CI workflow by Luna\n" +
 			"- Remove bintray support from plugin by Luna", result);
